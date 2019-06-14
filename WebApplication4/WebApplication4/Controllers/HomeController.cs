f5507b277dabcefadfcaf465830823d40c3b3539
reddivaripriyanka <45351226+reddivaripriyanka@users.noreply.github.com>
2019-06-14 16:55:18 +0530

    Update HomeController.cs

--- WebApplication4/WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebApplication4.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact details in ci/cd.";
 
             return View();
         }
